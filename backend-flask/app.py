0c3a6c28af999057c123e89555904d749694d2ea
spark0420 <spark@csustudent.net>
2023-03-08 20:39:00 +0000

    implement backend check for the token

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -3,6 +3,7 @@ from flask import request
 from flask_cors import CORS, cross_origin
 import os
 
+
 from services.home_activities import *
 from services.notifications_activities import *
 from services.user_activities import *
@@ -13,6 +14,9 @@ from services.message_groups import *
 from services.messages import *
 from services.create_message import *
 from services.show_activity import *
+
+#-----------------Cognito--------------------
+from lib.cognito_token import CognitoToken, extract_access_token, TokenVerifyError
 
 #-----------------HoneyComb--------------------
 from opentelemetry import trace
@@ -68,6 +72,15 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
+
+#-----------------Cognito--------------------
+cognito_token = CognitoToken(
+  user_pool_id = os.getenv("AWS_COGNITO_USER_POOL_ID"),
+  user_pool_client_id = os.getenv("AWS_COGNITO_USER_POOL_CLIENT_ID"),
+  region = os.getenv("AWS_DEFAULT_REGION")
+
+
+)
 #-----------------X-RAY--------------------
 XRayMiddleware(app, xray_recorder)
 
@@ -82,8 +95,8 @@ origins = [frontend, backend]
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
-  expose_headers="location,link",
-  allow_headers="content-type,if-modified-since",
+  headers=['Content-Type', 'Authorization'], 
+  expose_headers='Authorization',
   methods="OPTIONS,GET,HEAD,POST"
 )
 
@@ -158,7 +171,22 @@ def data_create_message():
 @xray_recorder.capture('activities_home')
 @cross_origin()
 def data_home():
-  data = HomeActivities.run()
+  access_token = extract_access_token(request.headers)
+  try:
+    claims = cognito_token.verify(access_token)
+    app.logger.debug('authenticated!')
+    app.logger.debug(claims)
+    app.logger.debug(claims['username'])
+    data = HomeActivities.run(cognito_user_id=claims['username'])
+  except TokenVerifyError as e:
+    app.logger.debug(e)
+    app.logger.debug('unauthenticated')
+    data = HomeActivities.run()
+    # _ = request.data
+    # abort(make_response(jsonify(message=str(e)), 401))
+
+  
+
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
