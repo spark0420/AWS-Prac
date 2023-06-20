cffd3d96c2cc98f8cd5937341f4e22f3b3a8501d
spark0420 <spark@csustudent.net>
2023-06-20 17:53:13 +0000

    Deleted Rollerbar

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -37,11 +37,11 @@ import watchtower
 import logging
 from time import strftime
 
-#-----------------Rollbar--------------------
-import os
-import rollbar
-import rollbar.contrib.flask
-from flask import got_request_exception
+# #-----------------Rollbar--------------------
+# import os
+# import rollbar
+# import rollbar.contrib.flask
+# from flask import got_request_exception
 
 # Configuring Logger to Use CloudWatch
 # I disables CloudWatch log because of the spend concern
@@ -105,29 +105,29 @@ cors = CORS(
 #     LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
 #     return response
 
-#-----------------Rollbar--------------------
-rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+# #-----------------Rollbar--------------------
+# rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
 
-@app.before_first_request
-def init_rollbar():
-    """init rollbar module"""
-    rollbar.init(
-        # access token
-        rollbar_access_token,
-        # environment name
-        'production',
-        # server root directory, makes tracebacks prettier
-        root=os.path.dirname(os.path.realpath(__file__)),
-        # flask already sets up logging
-        allow_logging_basic_config=False)
+# @app.before_first_request
+# def init_rollbar():
+#     """init rollbar module"""
+#     rollbar.init(
+#         # access token
+#         rollbar_access_token,
+#         # environment name
+#         'production',
+#         # server root directory, makes tracebacks prettier
+#         root=os.path.dirname(os.path.realpath(__file__)),
+#         # flask already sets up logging
+#         allow_logging_basic_config=False)
 
-    # send exceptions from `app` to rollbar, using flask's signal system.
-    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+#     # send exceptions from `app` to rollbar, using flask's signal system.
+#     got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
     
-@app.route('/rollbar/test')
-def rollbar_test():
-    rollbar.report_message('Hello World!', 'warning')
-    return "Hello World!"
+# @app.route('/rollbar/test')
+# def rollbar_test():
+#     rollbar.report_message('Hello World!', 'warning')
+#     return "Hello World!"
 
 @app.route("/api/message_groups", methods=['GET'])
 @cross_origin()
